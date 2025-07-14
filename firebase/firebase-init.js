98a8eb14dec89e103aea32be0a940019489c12f0
suneelsuthar <suneelsuther123@gmail.com>
2025-07-14 18:56:03 +0500

    updades

--- firebase/firebase-init.js
+++ firebase/firebase-init.js
@@ -1,12 +1,5 @@
-
 const firebaseConfig = {
-  apiKey: "AIzaSyCZmxebUjJV8jZMMbkqW9Q2dmojMb0eikE",
-  authDomain: "quizweb-13671.firebaseapp.com",
-  projectId: "quizweb-13671",
-  storageBucket: "quizweb-13671.firebasestorage.app",
-  messagingSenderId: "489855149015",
-  appId: "1:489855149015:web:48b0ebc9a8202b95df2192",
-  measurementId: "G-WEFP38LK03",
+  // add credentials here
 };
 
 // Initialize Firebase
